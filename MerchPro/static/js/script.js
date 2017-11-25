741c72acb67d9f4b5a3f4f3a8c8ade989d0c3e05
Dawn Wages <dawn.wages@gmail.com>
2017-11-25 01:26:10 -0500

    working on template... not getting far

--- MerchPro/static/js/script.js
+++ MerchPro/static/js/script.js
@@ -1,3 +1,30 @@
 document.documentElement.classList.remove("no-js");
 document.documentElement.classList.add("js");
 
+
+// Four images side by side
+function four() {
+    var elements = document.getElementsByClassName("column");
+    var i;
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.width = "25%";
+    }
+}
+
+// Two images side by side
+function two() {
+    var elements = document.getElementsByClassName("column");
+    var i;
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.width = "50%";
+    }
+}
+
+// Full-width images
+function one() {
+    var elements = document.getElementsByClassName("column");
+    var i;
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.width = "100%";
+    }
+}
